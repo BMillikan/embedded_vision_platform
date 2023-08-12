36ced7b674d8d15808179c0da9c8136d93ddbc31
Brian Millikan, Ph.D <brian.millikan@gmail.com>
2023-08-12 10:10:02 -0400

    remove build dir

--- build/third_party/libadsb/CMakeFiles/testadsb.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for testadsb.
-# This may be replaced when dependencies are built.
--- build/third_party/libadsb/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/data/embedded_vision_platform/third_party/libadsb
-# Build directory: /mnt/data/embedded_vision_platform/build/third_party/libadsb
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
